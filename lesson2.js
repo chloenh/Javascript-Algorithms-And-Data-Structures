611f6ac9bdede2a68d542ba7c3cc005706327205
chloenh <45791822+chloenh@users.noreply.github.com>
2019-02-21 21:33:31 +0100

    Create lesson2.js

---
+++ lesson2.js
@@ -0,0 +1,11 @@
+// In computer science, data is anything that is meaningful to the computer. JavaScript provides seven different data types 
+//which are undefined, null, boolean, string, symbol, number, and object.
+//Assign the value 7 to variable a.
+//Assign the contents of a to variable b.
+
+// Setup
+var a;
+var b = 2;
+
+var a = 7;
+var b = a;
