0a9c3a969caf566a093ebe7e22dda0a522d22b30
chloenh <45791822+chloenh@users.noreply.github.com>
2019-02-22 23:04:55 +0100

    Create lesson21.js

---
+++ lesson21.js
@@ -0,0 +1,8 @@
+//Assign the following three lines of text into the single variable myStr using escape sequences.
+
+//FirstLine
+//    \SecondLine
+//ThirdLine
+
+var myStr = "FirstLine\n\tSecondLine\\\nThirdLine"; 
+
