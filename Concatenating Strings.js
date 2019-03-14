38167fa17410b1e5de50caedaecb5ce91e15c045
chloenh <45791822+chloenh@users.noreply.github.com>
2019-03-14 20:53:15 +0100

    Create Concatenating Strings.js

---
+++ Concatenating Strings.js
@@ -0,0 +1,7 @@
+/* Task: Build myStr over 
+several lines by concatenating these two strings: 
+"This is the first sentence. " and "This is the 
+second sentence." */
+
+var myStr = "This is the first sentence.";
+myStr += " This is the second sentence."; 
